cbb68f93e7cec8e257467966b82cf330675d30ea
mohammed <mohammedwanir67@gmail.com>
2022-03-11 10:24:37 +0100

    backup

---
+++ E-LEARNING APPLICATION/Application E-learning  E-classe V1.0 (php)/student-list.php
@@ -0,0 +1,52 @@
+<?php 
+    $content = file_get_contents('students.json');
+    $students = json_decode($content,true);
+    $index=1;
+    if (! empty($students) ){
+    foreach($students as $student){
+?>
+<div class="row my-3 pt-3 border-none rounded-3 bg-white">
+    <div class="col ">
+        <div>
+            <img style="width: 90px;" src="./image/gens.jpg" alt="img">
+        </div>
+    </div>
+    <div class="col text-start pt-3">
+        <div>
+            <h6 class="text-muted "><?=  $student['name']?></h6>
+        </div>
+    </div>
+
+    <div class="col  text-start pt-3">
+        <div>
+            <h6 class="text-muted "><?= $student['email']?></h6>
+        </div>
+    </div>
+
+    <div class="col  text-start pt-3">
+        <div>
+            <h6 class="text-muted "><?= $student['phone']?></h6>
+        </div>
+    </div>
+
+    <div class="col  text-start pt-3">
+        <div>
+            <h6 class="text-muted "><?= $student['enroll_number']?></h6>
+        </div>
+    </div>
+
+    <div class="col-2  text-start pt-3">
+        <div>
+            <h6 class="text-muted "><?= $student['date_of_addmission']?></h6>
+        </div>
+    </div>
+    <div class="col pt-3 text-center">
+        <a href="edit.php"><i style="color:#00C1FE ;" class="bi bi-pencil pe-3"></i></a>
+        <a href="delet.php"> <i style="color:#00C1FE ;" class="bi bi-trash"></i></a>
+    </div>
+</div>
+<?php
+$index++;
+    }
+  }
+?>
